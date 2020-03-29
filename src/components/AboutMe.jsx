7a5214a33c12cb0ffd91f7b41807c32bc1ce754c
Clayton Lewis <lewisclayton89@gmail.com>
2020-03-29 18:53:14 -0400

    backplan

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,25 +1,55 @@
 import React from 'react';
 import aboutMeStyles from '../stylesCSS/aboutme.module.css';
-import hockeyPic from '../aboutMePics/willie.jpg';
+//import hockeyPic from '../aboutMePics/buffStatethrowBack.jpg';
+
 
 class AboutMe extends React.Component {
     constructor() {
         super() 
         this.state = {
-
+            name: "ClayDay",
+            picTest: "./Pictures/exclamationMark.jpg",
+            message: "You have an Alert",
+            backgroundColor: "black",       
         }
+        this.makeMessage = this.makeMessage.bind(this)    
     };
+
+    makeMessage() {
+         alert(this.state.message)
+    }
+
+    goHome(){
+        window.location.href="/"
+    }
+
+    /*
+    componentWillMount() {
+        alert('its begun')
+    }
+    
+   */
+
+
     render() {
+       
+
         return(
             <div>
                 <header>About Me</header>
 
                 <div className={aboutMeStyles.playBar}>
-                    <button>Browser Party</button>
-                    <button>Home</button>
-                    <button>Experience</button>
-                </div>
 
+                    <button onClick={this.makeMessage}>
+                        
+                    </button>
+
+                    <button onClick={this.goHome}>
+                        Go Home
+                    </button>
+                        
+                </div>
+                
                 <div className={aboutMeStyles.introStyle}>
                     Hello This is Clayton Lewis and i want to see if these words will stay in the damn box!
                     Thats all I really need this element to do, is to keep the words in the fucking square!
@@ -27,14 +57,11 @@ class AboutMe extends React.Component {
                     out the kinks on what I really want to say, but whatever it is will definitely be compelling. 
                     I look forward to publising this site on GitHub so I can showcase my portfolio to the world!
                     First thing is first! I need to concentrate on ReactJS. Right now that is the KEY!
-                    Stay tuned for the next additions! My goal is to turn this project into a CLASSIC! 
-                    You as the user will not be dissapointed! My goal in life it make and impact and 
-                    have a hot damn time,
-                    doing it!
+                  
                 </div>
 
                 <div className={aboutMeStyles.picHolder}>
-                    <img src={hockeyPic}  alt=""/>
+                    <img src={this.state.picTest}  alt=""/>
                 </div>
 
             </div>
