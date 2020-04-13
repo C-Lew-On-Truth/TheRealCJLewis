0e4526771f0f34cf40cd6ee418de260a0b1f3c4c
Clayton Lewis <lewisclayton89@gmail.com>
2020-04-12 22:23:16 -0400

    Please Work

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,21 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import Slider from "react-slick";
+import { Player, BigPlayButton } from "video-react";
 
 import '../stylesCSS/project.css';
-
-
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "video-react/dist/video-react.css"; // import css
 
-import Food from '../aboutMePics/Food.jpg';
-import Will from '../aboutMePics/Willie.jpg';
-import Amilion from '../aboutMePics/Amilion.jpg'
+import SundayVid from '../assets/Sunday-Skate-Video.mp4'
+import Food from '../aboutMePics/Food.JPG';
+import Will from '../aboutMePics/willie.jpg';
+import Amilion from '../aboutMePics/Amilion.JPG'
 import ThrowBack from '../aboutMePics/BsThrowBack.jpg';
 
 
-class Projects extends React.Component{
-    
-    
+class Projects extends React.Component {
+
+
     render() {
         const settings = {
             dots: true,
@@ -23,40 +24,44 @@ class Projects extends React.Component{
             speed: 500,
             slidesToShow: 1,
             slidesToScroll: 1
-          };
+        };
 
-        return(
+        return (
 
-         <div className="projectContainer">
+            <div className="projectContainer">
                 <header>Projects</header>
 
                 <div className="projectIntro">
-               
+
                 </div>
 
-            
-            <Slider {...settings} className="sliderContainer">
-                <div>
-                    <img className="slideProjects" src={Food} />
-                </div>
 
-                <div>
-                    <img className="slideProjects" src={Will} />
-                </div>
+                <Slider {...settings} className="sliderContainer">
+                    <Player src={SundayVid}>
+                        <BigPlayButton position="center" />
+                    </Player>
 
-                <div>
-                    <img className="slideProjects" src={Amilion} />
-                 </div>
+                    <div>
+                        <img className="slideProjects" src={Will} />
+                    </div>
 
-                <div>
-                    <img className="slideProjects" src={ThrowBack} />
-                </div>
-            </Slider>
-         
-            
-         </div>
-                    
-            
+                    <div>
+                        <img className="slideProjects" src={Will} />
+                    </div>
+
+                    <div>
+                        <img className="slideProjects" src={Amilion} />
+                    </div>
+
+                    <div>
+                        <img className="slideProjects" src={ThrowBack} />
+                    </div>
+                </Slider>
+
+
+            </div>
+
+
         )
     }
 }
